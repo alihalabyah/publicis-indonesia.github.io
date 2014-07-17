bd3cb66846f51ed073f6d5af1b1c5f3b96b23ae1
Alfiana E. Sibuea <alfian.sibuea@gmail.com>
2014-07-17 21:34:29 +0700

    Update content

--- main.js
+++ main.js
@@ -7,10 +7,10 @@
     var doc = $(window.document);
     var math = window.Math;
     
-    // Define application namespace
-    var app = {
+    // Define backgroundlication namespace
+    var background = {
         
-        // Application document properties
+        // backgroundlication document properties
         doc: {
             width: 0,
             height: 0
@@ -34,18 +34,18 @@
         resize: function() {
             
             // Resize document
-            app.doc.width = $(window).width();
-            app.doc.height = $(window).height();
+            background.doc.width = $(window).width();
+            background.doc.height = $(window).height();
             
             // Resize box
-            app.box.width = app.doc.width / 4;
-            app.box.height = app.doc.height / 4;
+            background.box.width = background.doc.width / 4;
+            background.box.height = background.doc.height / 4;
             
             // Resize each element
             $('#boxes span').each(function() {
                 
-                var width = app.box.width;
-                var height = app.box.height;
+                var width = background.box.width;
+                var height = background.box.height;
                 var x = $(this).data('x');
                 var y = $(this).data('y');
                 
@@ -65,9 +65,9 @@
         move: function(element, x, y) {
             
             // Get random type move, x first or y first
-            var type = app.randomNumber(0, 1);
-            var left = app.box.width * x;
-            var top  = app.box.height * y;
+            var type = background.randomNumber(0, 1);
+            var left = background.box.width * x;
+            var top  = background.box.height * y;
             
             // Coordinate X on the first move by default
             var first = 'left';
@@ -97,7 +97,7 @@
         // Initialize box position to starting coordinate
         init: function() {
             
-            // Append span to the boxes
+            // backgroundend span to the boxes
             var span = '';
             
             for (var a = 0; a < 16; a++) {
@@ -107,23 +107,23 @@
             $('#boxes').append(span);
             
             // Set properties value
-            app.resize();
+            background.resize();
             
             // Move to the first coordinate
             $('#boxes span').each(function(index) {
                 
                 var x = index % 4;
                 var y = parseInt(index/4);
-                var time = app.randomNumber(0, 2000);
+                var time = background.randomNumber(0, 2000);
                 var el = $(this);
                 
                 // Bind data id
                 el.attr('data-id', index);
                 
                 window.setTimeout(function() {
-                    app.move(el, x, y);
+                    background.move(el, x, y);
                     if (index === 15) {
-                        app.run();
+                        background.run();
                     }
                 }, time);
                 
@@ -134,16 +134,16 @@
         run: function() {
             
             // Get random numbers
-            var id = app.randomNumber(0, 15);
-            var x = app.randomNumber(0, 3);
-            var y = app.randomNumber(0, 3);
-            var time = app.randomNumber(1000, 2000);
+            var id = background.randomNumber(0, 15);
+            var x = background.randomNumber(0, 3);
+            var y = background.randomNumber(0, 3);
+            var time = background.randomNumber(1000, 2000);
             
             var element = $('#boxes span[data-id="'+id+'"]');
             
             window.setTimeout(function() {
-                app.move(element, x, y);  
-                app.run();
+                background.move(element, x, y);  
+                background.run();
             }, time);
             
         },
@@ -151,8 +151,11 @@
     
     doc.on('ready', function() {
         
-        app.init();
-        $(window).on('resize', app.resize);
+        background.init();
+        $(window).on('resize', background.resize);
+
+        var w = new Waves();
+        w.displayEffect();
         
     });
     
